4ff3d47dbec6fa7700ab9628d083ab48b90bc9a3
Constantine <aspid812@gmail.com>
2022-02-01 16:09:28 +0300

    Add application skeleton

---
+++ src/main/java/dev/aspid812/ridersoftdemo/RiderSoftDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.aspid812.ridersoftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderSoftDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiderSoftDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aspid812/ridersoftdemo/RiderSoftDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aspid812.ridersoftdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiderSoftDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
